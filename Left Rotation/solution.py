92470edb7fb7563ebe731897ccaebfef8ca953fb
ahsanscode <97919149+ahsanscode@users.noreply.github.com>
2023-06-13 21:30:04 +0600

    Create solution.py

---
+++ Left Rotation/solution.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    arrayx = [0]*d
+    for var in range(d):
+        arrayx[var] = arr[var]
+    for var in range(len(arr)-d):
+        arr[var] = arr[var+d]
+    for var in range(d):
+        arr[(len(arr) - d) + var ] = arrayx[var]
+    return arr
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
